23fe7a1ba5fcc9513fc9beaee8b8fb257f111b7f
Alex Polyachinscky <polya4inscky.alex@yandex.ru>
2020-12-29 14:53:51 +0300

    feat: added Localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,18 @@
 import { createStore, combineReducers } from "redux"
 import todoReducer from "./todoReducer"
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
 const reducers = combineReducers({ todo: todoReducer })
+const persistedState = localStorage.getItem("todoState")
+  ? JSON.parse(localStorage.getItem("todoState"))
+  : {}
 const store = createStore(
   reducers,
+  persistedState,
   // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+store.subscribe(() => {
+  localStorage.setItem("todoState", JSON.stringify(store.getState()))
+})
 window.store = store
 export default store
